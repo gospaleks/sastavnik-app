7b72e4364b40750094943e80b70d983d677e41d7
gospaleks <aleksandar.gospavic@elfak.rs>
2025-04-05 00:57:28 +0200

    not found page modifikovana i dodat card komponenta iz shadcnui

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,8 @@
-import { Button } from '@/components/ui/button';
-import { ArrowLeft } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import { Button, buttonVariants } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
 
 export const metadata = {
   title: '404 - Stranica nije pronađena',
@@ -8,20 +10,34 @@ export const metadata = {
 
 const NotFound = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 text-center dark:bg-gray-900">
-      <h1 className="text-6xl font-bold text-gray-900 dark:text-white">404</h1>
-      <p className="mt-4 text-xl text-gray-700 dark:text-gray-300">
-        Stranica nije pronađena
-      </p>
-      <p className="text-md mt-2 text-gray-600 dark:text-gray-400">
-        Izgleda da stranica koju tražite ne postoji.
-      </p>
-      <Button className="mt-6" asChild>
-        <Link href="/">
-          <ArrowLeft />
-          Vrati se na početnu
-        </Link>
-      </Button>
+    <div className="bg-accent flex min-h-screen flex-col items-center justify-center gap-4 text-center">
+      <div>
+        <h1 className="text-6xl font-bold text-gray-900 dark:text-white">
+          404
+        </h1>
+        <p className="text-xl text-gray-700 dark:text-gray-300">
+          Stranica nije pronađena
+        </p>
+        <p className="text-md text-gray-600 dark:text-gray-400">
+          Izgleda da stranica koju tražite ne postoji.
+        </p>
+      </div>
+
+      <Link href="/" className={buttonVariants({ size: 'lg' })}>
+        <ArrowLeft />
+        Vrati se na početnu
+      </Link>
+
+      <Link href="/">
+        <Image
+          src="/logo_navbar.png"
+          alt="Sastavnik"
+          width={100}
+          height={50}
+          className="h-auto w-auto cursor-pointer"
+          priority
+        />
+      </Link>
     </div>
   );
 };
